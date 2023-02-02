869c998f280e8e3d013d41f35b5882566966182b
Fayez Debbabi <fayezdebbabi@gmail.com>
2023-02-02 21:34:20 +0100

    Factor packages installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,28 @@
 
 # This script allows you to bootsrap a Linux machine that has nix installed with flakes experimental features enabled for now
 
-# Packages Installation section
+############### Packages Installation section
 
 
-# This is temporarly, need to refactor it with a simple array of these packages
-nix profile install nixpkgs#bat
-nix profile install nixpkgs#chafa
-nix profile install nixpkgs#cmus
-nix profile install nixpkgs#exa
-nix profile install nixpkgs#git
-nix profile install nixpkgs#htop
-nix profile install nixpkgs#jq
-nix profile install nixpkgs#neovim
-nix profile install nixpkgs#python
-nix profile install nixpkgs#tmux
-nix profile install nixpkgs#tree
-nix profile install nixpkgs#xclip
-nix profile install nixpkgs#yq
+# There are some issues with profiles, need to make sure how this stuff is more user friendly
+install() {
+  echo "Installing the packages..."
+  for package in "$@"
+  do
+	  echo "Installting $package"
+	  nix profile install nixpkgs#$package
+	  echo "Done installing $package"
+  done
+  echo "Done Installing packages."
+ 
+}
 
-# Configuration section
+
+install sl bat chafa cmus exa git htop jq neovim python tmux tree xclip yq
+
+
+
+############### Configuration section
 # 1. Need to clone my public repo
 # 2. Create a symoblic link creation function
 # 3. Create symlinks for the different configuration files in the good folders (Try to respect XDG Base Directory specification when possible)
