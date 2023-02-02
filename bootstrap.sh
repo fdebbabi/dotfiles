2fff17df767a43975eab6bbfe5f8bdfa54ba7116
Fayez Debbabi <fayezdebbabi@gmail.com>
2023-02-02 23:30:51 +0100

    Add git clone and partial config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,16 @@
 
 # This script allows you to bootsrap a Linux machine that has nix installed with flakes experimental features enabled for now
 
+############### Initilization
+
+WORKSPACE=$HOME/Workspace
+DOTFILES=$WORKSPACE/Git-Repos/dotfiles
+DOTS=$DOTFILES/configuration-files
+
+
+mkdir -p $WORKSPACE/{Git-Repos,Youtube,Other}
+mkdir -p $HOME/.config/{nvim,}
+
 ############### Packages Installation section
 
 
@@ -19,12 +29,56 @@ install() {
 }
 
 
-install sl bat chafa cmus exa git htop jq neovim python tmux tree xclip yq
+# install sl bat chafa cmus exa git htop jq neovim python tmux tree xclip yq
 
 
 
 ############### Configuration section
 # 1. Need to clone my public repo
+# This script needs to be idempotent (pour ne pas se faire chier s'il y a des choses qui existent ou autre.. x))
+git clone git@github.com:fdebbabi/dotfiles.git $DOTFILES
+
 # 2. Create a symoblic link creation function
+
+# This function is for now a 100% copy and paste from https://github.com/bew/dotfiles
+function make-link
+{
+  local link_to="$1"
+  local link_destination="$2"
+
+  # When linking `src/bar` to `foo/`
+  # $link_destination should be `foo/bar`
+  #
+  # NOTE: in bash, ${foo:(-N)} takes last N chars
+  # Ref: https://stackoverflow.com/a/19858692
+  if [[ "${link_destination:(-1)}" == "/" ]]; then
+    link_destination="${link_destination}$(basename "$link_to")"
+  fi
+
+  local old_link_to="$(readlink "$link_destination" || true)"
+  if [[ "$link_to" == "$old_link_to" ]]; then
+    echo "nothing to do, '$link_destination' already points to '$link_to'"
+    return
+  fi
+
+  if [[ "$DRYRUN_ONLY" == "true" ]]; then
+    local before before_short
+    if [[ -n "$old_link_to" ]]; then
+      before_short=R
+      before="was '$old_link_to'"
+    else
+      before_short=N
+      before="to create"
+    fi
+    echo "DRYRUN[$before_short]: '$link_destination' -> '$link_to' ($before)"
+  else
+    ln -vsf --no-dereference "$link_to" "$link_destination"
+  fi
+}
+
 # 3. Create symlinks for the different configuration files in the good folders (Try to respect XDG Base Directory specification when possible)
+# Testing with a single conf for now (neovim)
+make-link $DOTS/init.vim ~/.config/nvim/init.vim
+
+
 # 4. Not sure for now, but need to setup also vim plugins(like Cellular-automaton) and stuff (probably should feasable using the CLI)
