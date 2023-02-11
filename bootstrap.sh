7973e47e13c678596cac9734807f68398a89817c
Fayez Debbabi <fayezdebbabi@gmail.com>
2023-02-11 17:21:29 +0100

    Move fonts to single user mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ WORKSPACE=$HOME/Workspace
 DOTFILES=$WORKSPACE/Git-Repos/dotfiles
 DOTS=$DOTFILES/dots
 mkdir -p $WORKSPACE/{Git-Repos,Youtube,Other}
-mkdir -p $HOME/.config/{nvim,tmux,git,}
+mkdir -p $HOME/{.config/{nvim,tmux,git,oh-my-posh,},.fonts,}
 
 ################### PACKAGES INSTALLATION SECTION
 # There are some issues with profiles, need to make sure how this stuff is more user friendly
@@ -21,13 +21,19 @@ install() {
   echo "Done Installing packages."
 }
 
-#install sl bat chafa cmus exa git htop jq neovim python ranger tmux tree xclip yq zsh oh-my-zsh
+# install sl bat chafa cmus exa git htop jq neovim python3 ranger tmux tree xclip yq oh-my-zsh
+install_oh_my_posh() {
+	sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+	sudo chmod +x /usr/local/bin/oh-my-posh
+}
 
-if ! grep ".nix-profile/bin/zsh" /etc/shells 
-then
-	echo "Adding '$(which zsh)' to /etc/shells to be considered as a valid login shell..."
-    sudo -u root bash -c "echo $(which zsh) >> /etc/shells"
-fi
+# install_oh_my_posh
+
+# if ! grep ".nix-profile/bin/zsh" /etc/shells 
+# then
+# 	echo "Adding '$(which zsh)' to /etc/shells to be considered as a valid login shell..."
+#     sudo -u root bash -c "echo $(which zsh) >> /etc/shells"
+# fi
 
 
 ###################  CONFIGURATION SECTION 
@@ -40,8 +46,12 @@ fi
 
 ln -f -s -n $DOTS/init.vim ~/.config/nvim/init.vim
 ln -f -s -n $DOTS/tmux.conf ~/.config/tmux/tmux.conf
+ln -f -s -n $DOTS/"Meslo LG L DZ Bold Nerd Font Complete.ttf" ~/.fonts/"Meslo LG L DZ Bold Nerd Font Complete.ttf"
+ln -f -s -n $DOTS/fayez-theme.omp.json ~/.config/oh-my-posh/fayez-theme.omp.json
 ln -f -s -n $DOTS/zshrc ~/.zshrc
-ln -f -s -n $(nix profile list |grep "oh-my-zsh"  | awk '{print $4 "/share/oh-my-zsh"}') ~/.oh-my-zsh
+
+echo "Building font information caches..."
+fc-cache -f 
 
 echo "Updating login shell to ZSH..."
 chsh -s $(which zsh)
