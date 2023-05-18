c1c59ade6b9695f8c734e0a6c9324179349f6c52
Fayez DEBBABI <fayezdebbabi@gmail.com>
2023-05-18 12:21:34 +0200

    Clean some stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/zsh
 
 # This script requires nix package manager installed with flakes enabled and zsh shell available
 
@@ -8,7 +8,7 @@ WORKSPACE=$HOME/Workspace
 DOTFILES=$WORKSPACE/Git-Repos/dotfiles
 DOTS=$DOTFILES/dots
 mkdir -p $WORKSPACE/{Git-Repos,Youtube,Other}
-mkdir -p $HOME/{.config/{nvim,tmux,git,oh-my-posh,},.fonts,}
+mkdir -p $HOME/{.config/{nvim,tmux,git,},.fonts,}
 
 ################### PACKAGES INSTALLATION SECTION
 # There are some issues with profiles, need to make sure how this stuff is more user friendly
@@ -26,7 +26,10 @@ install() {
 # install sl bat chafa cmus exa htop jq neovim python3 ranger tmux tree xclip yq oh-my-zsh asciiquarium
 
 # install_oh_my_zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  --keep-zshrc
+rm -f ~/.zshrc
+rm -rf ~/.oh-my-zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 ###################  CONFIGURATION SECTION 
 if [ ! -d "$DOTFILES" ] ; then
@@ -43,10 +46,14 @@ ln -f -s -n $DOTS/"Meslo LG L DZ Bold Nerd Font Complete.ttf" ~/.fonts/"Meslo LG
 ln -f -s -n $DOTS/zshrc ~/.zshrc
 
 echo "Building font information caches..."
-# fc-cache -f 
+fc-cache -f 
 
-echo "Updating login shell to ZSH..."
-# chsh -s $(which zsh)
+if [[ $(getent passwd $USER |  cut -f 7 -d:) =~ "zsh" ]]; then
+	echo "ZSH is very likely a login shell, not going to do a thing"
+else
+	echo "ZSh is very likely not a login shell, updating login shell to ZSH..."
+	chsh -s $(which zsh)
+fi
 
 # 3. Not sure for now, but need to setup also vim plugins(like Cellular-automaton) and stuff (probably should feasable using the CLI)
 # Need to install Plug plugin manager, cellular-automaton and treesitter plugins with Python parser
